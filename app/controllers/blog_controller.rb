cf06a39ff1dc9759daeb3f3f0ffd81bcc536e281
pedro almeida <pedro.marc2003@gmail.com>
2019-06-30 16:21:38 -0300

    posting

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,32 @@
+class BlogController < ApplicationController
+  skip_before_action :authenticate_user!
+
+
+  def post0
+    @post = posts_all[0]
+  end
+
+  def post1
+    @post = posts_all[1]
+  end
+
+  def post2
+    @post = posts_all[2]
+  end
+
+  def post3
+     @post = posts_all[3]
+  end
+
+
+
+private
+
+def posts_all
+  postsYAML = YAML.load(ERB.new(File.read("config/post.yml")).result(binding))
+  posts_all = postsYAML.collect { |k, v| v }
+end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+#POSTS---------------------------------------------------------------------------------------------
+
+  get "bla-bla", to: 'blog#post0', as: :"bla-bla"
+  get "bla-bla2", to: 'blog#post1', as: :"bla-bla2"
+  get "bla-bla3", to: 'blog#post2', as: :"bla-bla3"
+
+
+
+
 end
