bb938947070c27b1d670ac7f106ca2f2208ca4e7
pedro almeida <pedro.marc2003@gmail.com>
2019-07-10 23:05:53 -0300

    postingblog4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @posts = posts_all
+    @posts = posts_all.reverse_each.to_h
     @firstpost = @posts["post0"]
     @secondposts = @posts
 
@@ -13,9 +13,6 @@ class PagesController < ApplicationController
   private
 
     def posts_all
-      postsYAML = YAML.load(ERB.new(File.read("config/post.yml")).result(binding))
-      posts_all = postsYAML.collect { |k, v| v }
-
       file = "config/post.yml"
       posts_all = YAML.load(open(file).read)
     end
